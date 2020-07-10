92bc95b3785f370e00b3a558ad3ed4716c38b1fe
Artem Konevskikh <akonevskikh@strelka-kb.com>
2020-07-11 02:43:41 +0300

    bear

--- js/video.js
+++ js/video.js
@@ -7,6 +7,8 @@ const video3 = document.getElementById('video-current');
 video3.pause();
 const video4 = document.getElementById('video-gan');
 video4.pause();
+const video5 = document.getElementById('video-bear');
+video5.pause();
 
 AFRAME.registerComponent('listener', {
   tick: function () {
@@ -18,22 +20,27 @@ AFRAME.registerComponent('listener', {
       video1.pause();
     }
 
-    if ((userPosition["z"] <= -12.3) && (userPosition["x"] > -25) && (userPosition["x"] > -25.55) && (userPosition["x"] < -11)) {
+    if ((userPosition["z"] <= -12.3) && (userPosition["z"] > -25) && (userPosition["x"] > -25.55) && (userPosition["x"] < -11)) {
       video2.play();
     } else {
       video2.pause();
     }
 
-    if ((userPosition["z"] <= -12.3) && (userPosition["x"] > -25) && (userPosition["x"] > -9) && (userPosition["x"] < 9.65)) {
+    if ((userPosition["z"] <= -12.3) && (userPosition["z"] > -25) && (userPosition["x"] > -9) && (userPosition["x"] < 9.65)) {
       video3.play();
     } else {
       video3.pause();
     }
 
-    if ((userPosition["z"] <= -12.3) && (userPosition["x"] > -25) && (userPosition["x"] > 11.6) && (userPosition["x"] < 26)) {
+    if ((userPosition["z"] <= -12.3) && (userPosition["z"] > -25) && (userPosition["x"] > 11.6) && (userPosition["x"] < 26)) {
       video4.play();
     } else {
       video4.pause();
+    }
+    if ((userPosition["z"] <= 15.4) && (userPosition["z"] > -25) && (userPosition["x"] > -25.55) && (userPosition["x"] < -4.7)) {
+      video5.play();
+    } else {
+      video5.pause();
     }
   }
 });
